6563689dd893d232e4e74b72d61f7d5596609c67
JUANCHO08730 <juancho08730@gmail.com>
2022-10-29 00:16:11 -0500

    fue nuevo cambio

--- 02-JS-I/homework/homework.js
+++ 02-JS-I/homework/homework.js
@@ -1,7 +1,7 @@
 // En estas primeras 6 preguntas, reemplaza `null` por la respuesta
 
 // Crea una variable "string", puede contener lo que quieras:
-const nuevaString = "HOLA JUAN MARTINEZ AGUILAR";
+const nuevaString = "HOLA JUAN MARTINEZ AGUILAR   esta es una segunda prueba";
 
 // Crea una variable numérica, puede ser cualquier número:
 const nuevoNum = null;
